3efb8ae3b5f759b2070d817fd9dae394f70c5055
HaiderAli228 <flutter2830@gmail.com>
2024-10-27 00:06:09 +0500

    adding api and api files

---
+++ lib/view-model/api_links.dart
@@ -0,0 +1,4 @@
+class ApiLinks{
+  static const String baseUrl = 'https://api.vyro.ai/v1/imagine/api/generations';
+  static const String postApiEndPoint = 'vk-NWDuDR9puZuplrtxaahCsUvKofx2r4eBkNZnMqEw3Ugree';
+}
